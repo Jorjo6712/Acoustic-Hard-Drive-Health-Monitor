b83b1f8cbe016ba3b10ec76d8839dc35e98690ad
Magnus Lund <magnusherringelund@gmail.com>
2026-08-17 12:54:20 +0200

    Added arduino project

---
+++ arduino/Config.Template.h
@@ -0,0 +1,14 @@
+// Wi-Fi
+#define WIFI_SSID "" 
+#define WIFI_PASSWORD ""
+#define WIFI_STATIC_IP ""
+#define WIFI_SUBNET_MASK ""
+#define WIFI_GATEWAY ""
+#define WIFI_DNS_SERVER ""
+
+// Pins
+#define RECORD_BUTTON_PIN 6
+#define RECORD_LIGHT_PIN 7
+
+// Server
+#define API_HOSTNAME "https://whatever.com"
