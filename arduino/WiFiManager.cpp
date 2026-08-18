f25c8c83873cca7aff1294bb3d3d1c29b7241f5a
Magnus Lund <magnusherringelund@gmail.com>
2026-08-18 08:40:29 +0200

    Created WiFiManager

--- arduino/WiFiManager.cpp
+++ arduino/WiFiManager.cpp
@@ -1,3 +1,49 @@
 #pragma once
 
+#include <WiFiS3.h>
 #include <Arduino.h>
+#include "WiFiManager.h"
+
+WiFiManager::WiFiManager(const char* ssid, const char* password, String localIp, String gateway, String subnet, String dnsServer = "1.1.1.1") 
+  : _ssid(ssid), _password(password) 
+{
+  if (!_localIp.fromString(localIp)) {
+    Serial.println("Invalid static IP!");
+  }
+  if (!_subnet.fromString(subnet)) {
+    Serial.println("Invalid subnet!");
+  }
+  if (!_gateway.fromString(gateway)) {
+    Serial.println("Invalid gateway!");
+  }
+  if (!_dnsServer.fromString(dnsServer)) {
+    Serial.println("Invalid DNS!");
+  }
+}
+
+void WiFiManager::connect()
+{
+  delay(10);
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.config(_localIp, _dnsServer, _gateway, _subnet);
+
+  WiFi.begin(_ssid, _password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void WiFiManager::ensureConnectivity()
+{
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi lost, reconnecting...");
+    connect();
+  }
+}
--- arduino/WiFiManager.h
+++ arduino/WiFiManager.h
@@ -1,3 +1,20 @@
 #pragma once
 
 #include <Arduino.h>
+#include <WiFiS3.h>
+
+class WiFiManager {
+  private:
+    const char* _ssid;
+    const char* _password;
+
+    IPAddress _localIp;
+    IPAddress _dnsServer;
+    IPAddress _gateway;
+    IPAddress _subnet;
+
+  public:
+    WiFiManager(const char* ssid, const char* password, String localIp, String gateway, String subnet, String dnsServer);
+    void connect();
+    void ensureConnectivity();
+};
